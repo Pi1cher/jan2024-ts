2790de63cd713cd51c7cc7e9a360ef0242e46b62
Pi1cher <skete2002@gmail.com>
2024-02-25 22:19:24 +0200

    hw3

---
+++ src/hoc/ContextProvider.tsx
@@ -0,0 +1,22 @@
+import {createContext, FC, PropsWithChildren} from 'react';
+const FirstContext = createContext<number>(null)
+const SecondContext = createContext<{ name:string }>(null)
+interface IProps extends PropsWithChildren{
+
+}
+
+const ContextProvider: FC<IProps> = ({children}) => {
+    return (
+        <FirstContext.Provider value={555}>
+            <SecondContext.Provider value={{name:'Max'}}>
+                {children}
+            </SecondContext.Provider>
+        </FirstContext.Provider>
+    );
+};
+
+export {
+    ContextProvider,
+    FirstContext,
+    SecondContext
+};
---
+++ src/services/apiService.ts
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+const apiService = axios.create({baseURL: 'sssss'})
+
+export {
+    apiService
+}
